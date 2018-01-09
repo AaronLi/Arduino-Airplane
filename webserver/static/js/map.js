439774d81fc435ee8dad49021fba91ce23fe0e9f
Albert Zhan <albertzhan@gmail.com>
2018-01-09 13:39:34 -0500

    Path drawing

--- webserver/static/js/map.js
+++ webserver/static/js/map.js
@@ -8,16 +8,31 @@ function initMap() {
     mapTypeId: "hybrid",
     mapTypeControl: false
   });
+  flightCoordinates = [
+      uluru
+    ];
+  flightPath = new google.maps.Polyline({
+    path: flightCoordinates,
+    //geodesic: true,
+    strokeColor: '#0000FF',
+    strokeOpacity: 1.0,
+    strokeWeight: 3,
+  });
+  flightPath.setMap(map);
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
   google.maps.event.addListener(map, 'click', function(e){
     console.log('Position:', e.latLng.toString());
+
     comSock.send(e.latLng.toString());
     var marker = new google.maps.Marker({
       position: e.latLng,
       map: map
     });
-  })
+    var path = flightPath.getPath();
+    path.push(e.latLng);
+    console.log(path);
+  })  
 }
