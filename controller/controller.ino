f4787972d3fb597df987c2aeb41ef0bfbb30f649
Albert Zhan <albertzhan@users.noreply.github.com>
2018-01-15 09:59:52 -0800

    formatting + location

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
- //The current radio controller
+//The current radio controller
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -13,15 +13,15 @@
 #define PACKET_SIZE 7
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-LiquidCrystal lcd(7,8,9,10,11,12);
-void setup() 
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+void setup()
 {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
   //while (!Serial);
   Serial.begin(115200);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.home();
   lcd.clear();
   lcd.noCursor();
@@ -35,7 +35,7 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
   delay(500);
   Serial.println("Reset radio");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     lcd.print("Failed");
@@ -49,7 +49,7 @@ void setup()
   lcd.clear();
   lcd.print("Setting freq");
   delay(500);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     lcd.print("Failed");
@@ -63,7 +63,7 @@ void setup()
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
   lcd.clear();
@@ -71,45 +71,45 @@ void setup()
 
 void loop()
 {
-    // Send a message to rf95_server
-    uint8_t radiopacket[PACKET_SIZE];
-    uint8_t messageType = 0;
-    uint8_t rollValue = map(analogRead(0), 0, 1023, 0, 180);
-    uint8_t pitchValue = map(analogRead(1),0,1023,0,180);
-    pitchValue = 180-pitchValue;
-    uint8_t throttleValue = map(analogRead(2),0,1023,0,3);
-    radiopacket[0] = (messageType<<3)+(throttleValue<<1)+(rollValue>>7);
-    radiopacket[1] = (rollValue<<1)+(pitchValue>>7);
-    radiopacket[2] = (pitchValue<<1);
-    lcd.home();
-    lcd.print("                ");
-    lcd.home();
-    lcd.print("T");
-    lcd.print(throttleValue);
-    lcd.print(" X");
-    lcd.print(rollValue);
-    lcd.setCursor(7,0);
-    lcd.print(" Y");
-    lcd.print(pitchValue);
-    rf95.send((uint8_t *)radiopacket, PACKET_SIZE);
-    if (rf95.available()){ 
-    // Should be a reply message for us now   
+  // Send a message to rf95_server
+  uint8_t radiopacket[PACKET_SIZE];
+  uint8_t messageType = 0;
+  uint8_t rollValue = map(analogRead(0), 0, 1023, 0, 180);
+  uint8_t pitchValue = map(analogRead(1), 0, 1023, 0, 180);
+  pitchValue = 180 - pitchValue;
+  uint8_t throttleValue = map(analogRead(2), 0, 1023, 0, 3);
+  radiopacket[0] = (messageType << 3) + (throttleValue << 1) + (rollValue >> 7);
+  radiopacket[1] = (rollValue << 1) + (pitchValue >> 7);
+  radiopacket[2] = (pitchValue << 1);
+  lcd.home();
+  lcd.print("                ");
+  lcd.home();
+  lcd.print("T");
+  lcd.print(throttleValue);
+  lcd.print(" X");
+  lcd.print(rollValue);
+  lcd.setCursor(7, 0);
+  lcd.print(" Y");
+  lcd.print(pitchValue);
+  rf95.send((uint8_t *)radiopacket, PACKET_SIZE);
+  if (rf95.available()) {
+    // Should be a reply message for us now
     // Now wait for a reply
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
-   {
+    {
       long longitudeIn, latitudeIn;
-      longitudeIn+=((buf[0]&7)<<22)+(buf[1]<<14)+(buf[2]<<6)+buf[3]>>2;
-      latitudeIn+=((buf[3]&3)<<24)+(buf[4]<<16)+(buf[5]<<8)+buf[6];
-      longitudeIn-=18000000;
-      latitudeIn-=9000000;
-      lcd.setCursor(0,1);
+      longitudeIn += ((buf[0] & 7) << 22) + (buf[1] << 14) + (buf[2] << 6) + buf[3] >> 2;
+      latitudeIn += ((buf[3] & 3) << 24) + (buf[4] << 16) + (buf[5] << 8) + buf[6];
+      longitudeIn -= 18000000;
+      latitudeIn -= 9000000;
+      lcd.setCursor(0, 1);
       lcd.print("reply: ");
       lcd.print((char*)buf);
-      Serial.print(latitudeIn);Serial.print(" ");Serial.println(longitudeIn);
+      Serial.print(latitudeIn); Serial.print(" "); Serial.println(longitudeIn);
       //Serial.print("RSSI: ");
-      //Serial.println(rf95.lastRssi(), DEC);    
+      //Serial.println(rf95.lastRssi(), DEC);
     }
     else
     {
