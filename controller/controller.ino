04a32aca481e4f833e526c533697fafa25b77b4d
Albert Zhan <albertzhan@users.noreply.github.com>
2018-01-16 10:07:01 -0800

    Format + bug
    Please click ctrl + t so i can read code :)

--- controller/controller.ino
+++ controller/controller.ino
@@ -4,7 +4,7 @@
 
 
 
- //The current radio controller
+//The current radio controller
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -23,14 +23,14 @@ uint8_t throttle = 0;
 boolean cPressed = false;
 boolean zPressed = false;
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-void setup() 
+void setup()
 {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
   nck.initialize();
   //while (!Serial);
   Serial.begin(115200);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.home();
   lcd.clear();
   lcd.noCursor();
@@ -44,7 +44,7 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
   delay(500);
   Serial.println("Reset radio");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     lcd.print("Failed");
@@ -58,7 +58,7 @@ void setup()
   lcd.clear();
   lcd.print("Setting freq");
   delay(500);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     lcd.print("Failed");
@@ -72,7 +72,7 @@ void setup()
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
   lcd.clear();
@@ -80,67 +80,67 @@ void setup()
 
 void loop()
 {
-    // Send a message to rf95_server
-    nck.update();
-    uint8_t radiopacket[PACKET_SIZE];
-    uint8_t messageType = 0;
-    uint8_t rollValue = map(nck.joystick_x(), 27, 229, 0, 180);
-    uint8_t pitchValue = map(nck.joystick_y(),33,225,0,180);
-    pitchValue = 180-pitchValue;
-   if(nck.c_button()){
-    if(!cPressed){
+  // Send a message to rf95_server
+  nck.update();
+  uint8_t radiopacket[PACKET_SIZE];
+  uint8_t messageType = 0;
+  uint8_t rollValue = map(nck.joystick_x(), 27, 229, 0, 180);
+  uint8_t pitchValue = map(nck.joystick_y(), 33, 225, 0, 180);
+  pitchValue = 180 - pitchValue;
+  if (nck.c_button()) {
+    if (!cPressed) {
       cPressed = true;
-      if(throttle<throttleValues){
+      if (throttle < throttleValues) {
         throttle++;
       }
     }
-   }
-   else{
+  }
+  else {
     cPressed = false;
-   }
-   if(nck.z_button()){
-    if(!zPressed){
+  }
+  if (nck.z_button()) {
+    if (!zPressed) {
       zPressed = true;
-      if(throttle>0){
+      if (throttle > 0) {
         throttle--;
       }
     }
-   }
-   else{
+  }
+  else {
     zPressed = false;
-   }
-    radiopacket[0] = (messageType<<3)+(throttle<<1)+(rollValue>>7);
-    radiopacket[1] = (rollValue<<1)+(pitchValue>>7);
-    radiopacket[2] = (pitchValue<<1);
-    lcd.home();
-    lcd.print("                ");
-    lcd.home();
-    lcd.print("T");
-    lcd.print(throttle);
-    lcd.print(" X");
-    lcd.print(rollValue);
-    lcd.setCursor(7,0);
-    lcd.print(" Y");
-    lcd.print(pitchValue);
-    rf95.send((uint8_t *)radiopacket, PACKET_SIZE);
-    if (rf95.available()){ 
-    // Should be a reply message for us now   
+  }
+  radiopacket[0] = (messageType << 3) + (throttle << 1) + (rollValue >> 7);
+  radiopacket[1] = (rollValue << 1) + (pitchValue >> 7);
+  radiopacket[2] = (pitchValue << 1);
+  lcd.home();
+  lcd.print("                ");
+  lcd.home();
+  lcd.print("T");
+  lcd.print(throttle);
+  lcd.print(" X");
+  lcd.print(rollValue);
+  lcd.setCursor(7, 0);
+  lcd.print(" Y");
+  lcd.print(pitchValue);
+  rf95.send((uint8_t *)radiopacket, PACKET_SIZE);
+  if (rf95.available()) {
+    // Should be a reply message for us now
     // Now wait for a reply
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
-   {
+    {
       long longitudeIn, latitudeIn;
-      longitudeIn+=((buf[0]&7)<<22)+(buf[1]<<14)+(buf[2]<<6)+buf[3]>>2;
-      latitudeIn+=((buf[3]&3)<<24)+(buf[4]<<16)+(buf[5]<<8)+buf[6];
-      longitudeIn-=18000000;
-      latitudeIn-=9000000;
-      lcd.setCursor(0,1);
+      longitudeIn += ((buf[0] & 7) << 22) + (buf[1] << 14) + (buf[2] << 6) + buf[3] >> 2;
+      latitudeIn += ((buf[3] & 3) << 24) + (buf[4] << 16) + (buf[5] << 8) + buf[6];
+      longitudeIn -= 18000000;
+      latitudeIn -= 9000000;
+      lcd.setCursor(0, 1);
       lcd.print("reply: ");
       lcd.print((char*)buf);
-      Serial.print("Received: ");Serial.print(latitudeIn);Serial.print(" ");Serial.println(longitudeIn);
+      Serial.print("Received: "); Serial.print(latitudeIn); Serial.print(" "); Serial.println(longitudeIn);
       //Serial.print("RSSI: ");
-      //Serial.println(rf95.lastRssi(), DEC);    
+      //Serial.println(rf95.lastRssi(), DEC);
     }
     else
     {
--- webserver/static/js/map.js
+++ webserver/static/js/map.js
@@ -37,7 +37,7 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-  }
+
 
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
