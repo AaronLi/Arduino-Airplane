a5403da28eb6d9ae27b21b25638b6d77cea06987
Albert Zhan <albertzhan@users.noreply.github.com>
2018-01-18 12:22:38 -0800

    prettifying

--- webserver/main.py
+++ webserver/main.py
@@ -16,25 +16,25 @@ plane = None
 def connectSerial():
     global ser, plane
     availablePorts = serial.tools.list_ports.comports()
-    print("Ports:")
+    print(">>>>>>>>>>>>>>>>Ports:")
     for i in availablePorts:
-        print("\t%s, %s" % (i.device, i.description))
+        print(">>>>>>>>>>>>>>>>\t%s, %s" % (i.device, i.description))
     if len(availablePorts)>0:
         try:
-            print("Attempting to connect to port", availablePorts[0].device)
+            print(">>>>>>>>>>>>>>>>Attempting to connect to port", availablePorts[0].device)
             ser = serial.Serial(availablePorts[0].device, 115200, timeout=1/callbackHz)
             plane = airplane.Airplane(ser)
-            print("\tIsOpen:", ser.is_open, "port:", ser.name)
+            print(">>>>>>>>>>>>>>>>\tIsOpen:", ser.is_open, "port:", ser.name)
         except serial.serialutil.SerialException:
-            print("\tPort unavailable")
+            print(">>>>>>>>>>>>>>>>\tPort unavailable")
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         if client == None:
-            print("accessed")
+            print("----------------accessed")
             self.render("index.html")
         else:
-            print("client rejected")
+            print("----------------client rejected")
             self.write('''
 Client already connected
 <br/>
@@ -44,21 +44,21 @@ Think this is an error? <i> You might be right</i>
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self, *args, **kwargs):
         global client
-        print("Websocket opened",args,kwargs)
+        print("----------------Websocket opened",args,kwargs)
         client = self
     def on_close(self):
         global client
-        print("Websocket closed")
+        print("----------------Websocket closed")
         client = None
     def on_message(self, message):
-        print("Message received: ",message)
+        print("----------------Message received: ",message)
         smessages = message.split()
         if smessages[0] == 'Position:':
             coords.append(smessages[1])
         #temp
         if message != 'Hello!':
             latLongOut = [float(i) for i in message.replace('(','').replace(')','').split(',')]
-            print(latLongOut)
+            print("================",latLongOut)
             plane.send_gps_coordinate(latLongOut[0],latLongOut[1])
 
 def handleSerial():
@@ -67,14 +67,14 @@ def handleSerial():
         try:
             readData = ser.readline()
             if len(readData) != 0:
-                print('dataIn:',readData)
+                print('<<<<<<<<<<<<<<<<dataIn:',readData)
                 if client != None:
                     dataIn = str(list(readData[:-2])).replace('[','').replace(']','').replace(',','') #remove the \r\n
-                    print('decoded data:',dataIn,'\n^ compare with hex being written to arduino ^')
+                    print('<<<<<<<<<<<<<<<<decoded data:',dataIn,'\n^ compare with hex being written to arduino ^')
                     client.write_message(dataIn) # bytes represented in decimal
         except serial.serialutil.SerialException:
             #return
-            print("Device has probably been disconnected")
+            print("<<<<<<<<<<<<<<<<Device has probably been disconnected")
             connectSerial()
     else:
         #return
